8062c78d33644206814666d3e348251bd928ea2d
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-05-29 20:04:52 -0300

    vigesimo segundo exercicio

--- main.c
+++ main.c
@@ -13,52 +13,60 @@ No* criar_no(int valor) {
     return n;
 }
 
-void remover_todas_ocorrencias(No** inicio, int valor) {
-    while (*inicio && (*inicio)->dado == valor) {
-        No* temp = *inicio;
-        *inicio = (*inicio)->prox;
-        free(temp);
-    }
-    No* atual = *inicio;
-    while (atual && atual->prox) {
-        if (atual->prox->dado == valor) {
-            No* temp = atual->prox;
-            atual->prox = temp->prox;
-            free(temp);
-        } else {
-            atual = atual->prox;
-        }
-    }
-}
-
-void imprimir_lista(No* l) {
+int tamanho_lista(No* l) {
+    int cont = 0;
     while (l) {
-        printf("%d -> ", l->dado);
+        cont++;
         l = l->prox;
     }
-    printf("NULL\n");
+    return cont;
 }
 
-void liberar_lista(No* l) {
-    while (l) {
-        No* temp = l;
-        l = l->prox;
-        free(temp);
+No* encontrar_intersecao(No* l1, No* l2) {
+    int t1 = tamanho_lista(l1);
+    int t2 = tamanho_lista(l2);
+    int diff = t1 - t2;
+    if (diff < 0) {
+        No* temp = l1; l1 = l2; l2 = temp;
+        diff = -diff;
     }
+    for (int i = 0; i < diff; i++) {
+        l1 = l1->prox;
+    }
+    while (l1 && l2) {
+        if (l1 == l2) return l1;
+        l1 = l1->prox;
+        l2 = l2->prox;
+    }
+    return NULL;
 }
 
 int main() {
-    No* lista = criar_no(1);
-    lista->prox = criar_no(2);
-    lista->prox->prox = criar_no(3);
-    lista->prox->prox->prox = criar_no(2);
-    lista->prox->prox->prox->prox = criar_no(4);
-    lista->prox->prox->prox->prox->prox = criar_no(2);
+    No* comum = criar_no(7);
+    comum->prox = criar_no(8);
+    comum->prox->prox = criar_no(9);
 
-    imprimir_lista(lista);
-    remover_todas_ocorrencias(&lista, 2);
-    imprimir_lista(lista);
+    No* l1 = criar_no(1);
+    l1->prox = criar_no(2);
+    l1->prox->prox = criar_no(3);
+    l1->prox->prox->prox = comum;
 
-    liberar_lista(lista);
+    No* l2 = criar_no(4);
+    l2->prox = criar_no(5);
+    l2->prox->prox = comum;
+
+    No* intersecao = encontrar_intersecao(l1, l2);
+    if (intersecao) printf("%d\n", intersecao->dado);
+    else printf("0\n");
+
+    free(l1->prox->prox);
+    free(l1->prox);
+    free(l1);
+    free(l2->prox);
+    free(l2);
+    free(comum->prox->prox);
+    free(comum->prox);
+    free(comum);
+
     return 0;
 }
