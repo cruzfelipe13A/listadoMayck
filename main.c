0cc93cc63e66568b918e3e8b6bc307f611d62b69
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-05-29 19:42:32 -0300

    oitavo exercicio

--- main.c
+++ main.c
@@ -6,6 +6,27 @@ typedef struct No {
     struct No* prox;
 } No;
 
+typedef struct Pilha {
+    int valor;
+    struct Pilha* prox;
+} Pilha;
+
+void empilhar(Pilha** topo, int v) {
+    Pilha* novo = malloc(sizeof(Pilha));
+    novo->valor = v;
+    novo->prox = *topo;
+    *topo = novo;
+}
+
+int desempilhar(Pilha** topo) {
+    if (*topo == NULL) return -1;
+    Pilha* temp = *topo;
+    int v = temp->valor;
+    *topo = temp->prox;
+    free(temp);
+    return v;
+}
+
 void inserir_fim(No** l, int v) {
     No* n = malloc(sizeof(No));
     n->dado = v;
@@ -27,48 +48,33 @@ void imprimir(No* l) {
     printf("NULL\n");
 }
 
-No* encontrar_meio(No* l) {
-    if (l == NULL) return NULL;
-    No* lento = l;
-    No* rapido = l;
-    while (rapido && rapido->prox) {
-        lento = lento->prox;
-        rapido = rapido->prox->prox;
+void imprimir_reverso_com_pilha(No* l) {
+    Pilha* pilha = NULL;
+    while (l) {
+        empilhar(&pilha, l->dado);
+        l = l->prox;
     }
-    return lento;
+    while (pilha) {
+        printf("%d -> ", desempilhar(&pilha));
+    }
+    printf("NULL\n");
 }
 
 int main() {
     No* lista1 = NULL;
-    No* meio1 = encontrar_meio(lista1);
-    if (meio1)
-        printf("%d\n", meio1->dado);
-    else
-        printf("NULL\n");
+    imprimir_reverso_com_pilha(lista1);
 
     No* lista2 = NULL;
-    inserir_fim(&lista2, 10);
-    No* meio2 = encontrar_meio(lista2);
-    printf("%d\n", meio2->dado);
+    inserir_fim(&lista2, 42);
+    imprimir_reverso_com_pilha(lista2);
 
     No* lista3 = NULL;
     inserir_fim(&lista3, 1);
     inserir_fim(&lista3, 2);
     inserir_fim(&lista3, 3);
     inserir_fim(&lista3, 4);
-    inserir_fim(&lista3, 5);
     imprimir(lista3);
-    No* meio3 = encontrar_meio(lista3);
-    printf("%d\n", meio3->dado);
-
-    No* lista4 = NULL;
-    inserir_fim(&lista4, 10);
-    inserir_fim(&lista4, 20);
-    inserir_fim(&lista4, 30);
-    inserir_fim(&lista4, 40);
-    imprimir(lista4);
-    No* meio4 = encontrar_meio(lista4);
-    printf("%d\n", meio4->dado);
+    imprimir_reverso_com_pilha(lista3);
 
     return 0;
 }
