71da0afc2eb5e16ab44dc4bd78bd1f735a09d4e6
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-05-29 19:41:15 -0300

    setimo exercicio

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 typedef struct No {
-    int val;
+    int dado;
     struct No* prox;
 } No;
 
 void inserir_fim(No** l, int v) {
     No* n = malloc(sizeof(No));
-    n->val = v;
+    n->dado = v;
     n->prox = NULL;
     if (*l == NULL) {
         *l = n;
@@ -21,32 +21,54 @@ void inserir_fim(No** l, int v) {
 
 void imprimir(No* l) {
     while (l) {
-        printf("%d -> ", l->val);
+        printf("%d -> ", l->dado);
         l = l->prox;
     }
     printf("NULL\n");
 }
 
-void inverter_lista(No** l) {
-    No *ant = NULL, *atual = *l, *prox = NULL;
-    while (atual) {
-        prox = atual->prox;
-        atual->prox = ant;
-        ant = atual;
-        atual = prox;
+No* encontrar_meio(No* l) {
+    if (l == NULL) return NULL;
+    No* lento = l;
+    No* rapido = l;
+    while (rapido && rapido->prox) {
+        lento = lento->prox;
+        rapido = rapido->prox->prox;
     }
-    *l = ant;
+    return lento;
 }
 
 int main() {
-    No* lista = NULL;
-    inserir_fim(&lista, 1);
-    inserir_fim(&lista, 2);
-    inserir_fim(&lista, 3);
+    No* lista1 = NULL;
+    No* meio1 = encontrar_meio(lista1);
+    if (meio1)
+        printf("%d\n", meio1->dado);
+    else
+        printf("NULL\n");
 
-    imprimir(lista);       // 1 -> 2 -> 3 -> NULL
-    inverter_lista(&lista);
-    imprimir(lista);       // 3 -> 2 -> 1 -> NULL
+    No* lista2 = NULL;
+    inserir_fim(&lista2, 10);
+    No* meio2 = encontrar_meio(lista2);
+    printf("%d\n", meio2->dado);
+
+    No* lista3 = NULL;
+    inserir_fim(&lista3, 1);
+    inserir_fim(&lista3, 2);
+    inserir_fim(&lista3, 3);
+    inserir_fim(&lista3, 4);
+    inserir_fim(&lista3, 5);
+    imprimir(lista3);
+    No* meio3 = encontrar_meio(lista3);
+    printf("%d\n", meio3->dado);
+
+    No* lista4 = NULL;
+    inserir_fim(&lista4, 10);
+    inserir_fim(&lista4, 20);
+    inserir_fim(&lista4, 30);
+    inserir_fim(&lista4, 40);
+    imprimir(lista4);
+    No* meio4 = encontrar_meio(lista4);
+    printf("%d\n", meio4->dado);
 
     return 0;
 }
