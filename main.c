8a543d5beb904aef869d2ed8e02537a6ff8fdae5
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-05-29 19:46:38 -0300

    decimo exercicio

--- main.c
+++ main.c
@@ -1,84 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAMANHO_PILHA 100
-
 typedef struct No {
     int dado;
     struct No* prox;
 } No;
 
-typedef struct {
-    int dados[TAMANHO_PILHA];
-    int topo;
-} PilhaArray;
+typedef struct Pilha {
+    No* topo;
+} Pilha;
 
-void inicializar_pilha(PilhaArray* p) {
-    p->topo = -1;
+void inicializar_pilha(Pilha* p) {
+    p->topo = NULL;
 }
 
-int empilhar(PilhaArray* p, int valor) {
-    if (p->topo >= TAMANHO_PILHA - 1) return 0;
-    p->dados[++(p->topo)] = valor;
+int pilha_vazia(Pilha* p) {
+    return p->topo == NULL;
+}
+
+void empilhar(Pilha* p, int valor) {
+    No* novo = malloc(sizeof(No));
+    novo->dado = valor;
+    novo->prox = p->topo;
+    p->topo = novo;
+}
+
+int desempilhar(Pilha* p, int* valor) {
+    if (pilha_vazia(p)) return 0;
+    No* temp = p->topo;
+    *valor = temp->dado;
+    p->topo = temp->prox;
+    free(temp);
     return 1;
 }
 
-int desempilhar(PilhaArray* p, int* valor) {
-    if (p->topo < 0) return 0;
-    *valor = p->dados[(p->topo)--];
-    return 1;
-}
-
-void inserir_fim(No** l, int v) {
-    No* n = malloc(sizeof(No));
-    n->dado = v;
-    n->prox = NULL;
-    if (*l == NULL) {
-        *l = n;
-    } else {
-        No* a = *l;
-        while (a->prox) a = a->prox;
-        a->prox = n;
-    }
-}
-
-void imprimir(No* l) {
-    while (l) {
-        printf("%d -> ", l->dado);
-        l = l->prox;
-    }
-    printf("NULL\n");
-}
-
-void imprimir_reverso_com_array(No* l) {
-    PilhaArray pilha;
-    inicializar_pilha(&pilha);
-    while (l) {
-        empilhar(&pilha, l->dado);
-        l = l->prox;
-    }
-    int valor;
-    while (desempilhar(&pilha, &valor)) {
-        printf("%d -> ", valor);
+void imprimir_pilha(Pilha* p) {
+    No* atual = p->topo;
+    while (atual) {
+        printf("%d -> ", atual->dado);
+        atual = atual->prox;
     }
     printf("NULL\n");
 }
 
 int main() {
-    No* lista1 = NULL;
-    imprimir_reverso_com_array(lista1);
+    Pilha p;
+    inicializar_pilha(&p);
 
-    No* lista2 = NULL;
-    inserir_fim(&lista2, 7);
-    imprimir_reverso_com_array(lista2);
+    int valor;
 
-    No* lista3 = NULL;
-    inserir_fim(&lista3, 10);
-    inserir_fim(&lista3, 20);
-    inserir_fim(&lista3, 30);
-    inserir_fim(&lista3, 40);
-    imprimir(lista3);
-    imprimir_reverso_com_array(lista3);
+    desempilhar(&p, &valor);
+
+    empilhar(&p, 5);
+    imprimir_pilha(&p);
+
+    empilhar(&p, 10);
+    empilhar(&p, 15);
+    imprimir_pilha(&p);
+
+    desempilhar(&p, &valor);
+    printf("Desempilhado: %d\n", valor);
+    imprimir_pilha(&p);
 
     return 0;
 }
