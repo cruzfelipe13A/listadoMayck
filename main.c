b4bbc2ccc2ea71df255a6e53e57c831a579593bc
cruzfelipe13A <cruzfelipe585@gmail.com>
2025-05-29 20:07:36 -0300

    vigesimo quarto exercicio

--- main.c
+++ main.c
@@ -2,78 +2,59 @@
 #include <stdlib.h>
 
 typedef struct No {
-    int dado;
+    int valor;
+    int minimo;
     struct No* prox;
 } No;
 
-No* criar_no(int valor) {
+typedef struct Pilha {
+    No* topo;
+} Pilha;
+
+void inicializar(Pilha* p) {
+    p->topo = NULL;
+}
+
+void push(Pilha* p, int valor) {
     No* n = malloc(sizeof(No));
-    n->dado = valor;
-    n->prox = NULL;
-    return n;
+    n->valor = valor;
+    n->minimo = (!p->topo || valor < p->topo->minimo) ? valor : p->topo->minimo;
+    n->prox = p->topo;
+    p->topo = n;
 }
 
-int tamanho_lista(No* l) {
-    int cont = 0;
-    while (l) {
-        cont++;
-        l = l->prox;
-    }
-    return cont;
+int pop(Pilha* p) {
+    if (!p->topo) return -1;
+    No* temp = p->topo;
+    int valor = temp->valor;
+    p->topo = temp->prox;
+    free(temp);
+    return valor;
 }
 
-void rotacionar_lista(No** inicio, int k) {
-    if (!*inicio || k == 0) return;
-
-    int n = tamanho_lista(*inicio);
-    k = k % n;
-    if (k == 0) return;
-
-    No* atual = *inicio;
-    int count = 1;
-    while (count < n - k) {
-        atual = atual->prox;
-        count++;
-    }
-
-    No* nova_cabeca = atual->prox;
-    atual->prox = NULL;
-
-    No* fim = nova_cabeca;
-    while (fim->prox) {
-        fim = fim->prox;
-    }
-    fim->prox = *inicio;
-    *inicio = nova_cabeca;
+int top(Pilha* p) {
+    if (!p->topo) return -1;
+    return p->topo->valor;
 }
 
-void imprimir_lista(No* l) {
-    while (l) {
-        printf("%d -> ", l->dado);
-        l = l->prox;
-    }
-    printf("NULL\n");
-}
-
-void liberar_lista(No* l) {
-    while (l) {
-        No* temp = l;
-        l = l->prox;
-        free(temp);
-    }
+int obter_minimo(Pilha* p) {
+    if (!p->topo) return -1;
+    return p->topo->minimo;
 }
 
 int main() {
-    No* lista = criar_no(1);
-    lista->prox = criar_no(2);
-    lista->prox->prox = criar_no(3);
-    lista->prox->prox->prox = criar_no(4);
-    lista->prox->prox->prox->prox = criar_no(5);
-
-    imprimir_lista(lista);
-    rotacionar_lista(&lista, 2);
-    imprimir_lista(lista);
-
-    liberar_lista(lista);
+    Pilha p;
+    inicializar(&p);
+    push(&p, 3);
+    push(&p, 5);
+    printf("%d\n", obter_minimo(&p));
+    push(&p, 2);
+    push(&p, 1);
+    printf("%d\n", obter_minimo(&p));
+    pop(&p);
+    printf("%d\n", obter_minimo(&p));
+    pop(&p);
+    printf("%d\n", top(&p));
+    printf("%d\n", obter_minimo(&p));
     return 0;
 }
